1be8ab974a90cbfff7fc2901743c74660677bd52
Pranav Bhatia <pranav.bhatia@gogap.co>
2019-02-08 11:55:13 -0500

    exercise 5 added

--- February 8, 2019/script.js
+++ February 8, 2019/script.js
@@ -1,5 +1,29 @@
-4 + 3;
-
-if (4 + 3 === 7) {
+function sayHello(){
     console.log("Hello");
 }
+
+// sayHello();
+
+// Function Expression =>
+// This function is also called anonymous function.
+var sayBye = function() {
+    console.log("Bye");
+}
+
+// sayBye();
+
+// 
+function sing(song) {
+    console.log(song);
+}
+
+sing("Saa Ree Gaa");
+sing("Despacito");
+
+
+function multiply(a, b) {
+    return a * b;
+    // return a;
+}
+
+multiply(5, 10);
