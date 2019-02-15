a8eaf0bc0cca32be39dc52bf39c8c7d784d626c1
PranavBhatia <pranav.bhatia@gogap.co>
2019-02-15 14:31:19 -0500

    DOM files added

---
+++ Febraury 15, 2019/DOM Manipulation/main.js
@@ -0,0 +1,22 @@
+// document.getElementsByClassName("second")
+// document.getElementById("first")
+// document.getElementsByClassName("second")[0]
+// document.querySelector("h1")
+// document.querySelector("li")
+// document.querySelectorAll("li")
+// document.querySelectorAll("li, h1")
+// document.querySelector("li").getAttribute("random")
+// document.querySelector("li").setAttribute("random", "1000")
+// document.querySelector("li").getAttribute("random")
+// document.querySelector("h1").style.background = "yellow"
+
+// STYLES = >
+// var h1 = document.querySelector("h1")
+// h1.className = "coolTitle"
+// document.querySelector("li").classList
+// document.querySelector("li").classList.add("coolTitle")
+// document.querySelector("li").classList.remove("coolTitle")
+
+// BONUS = >
+// h1.innerHTML = "<strong>!!!</strong>"
+// document.querySelectorAll("li")[1].parentElement
