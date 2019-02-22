78d61a0fe93820c80672dcd60135e0b55080061d
PranavBhatia <bhatiapranav1996@gmail.com>
2019-02-22 11:50:16 -0500

    static child li append

--- February 22, 2019/DOM Events/script.js
+++ February 22, 2019/DOM Events/script.js
@@ -1,6 +1,9 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userInput");
+var ul = document.querySelector("ul");
 
 button.addEventListener("click", function() {
-    console.log("click is working");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode("Testing"));
+    ul.appendChild(li);
 })
