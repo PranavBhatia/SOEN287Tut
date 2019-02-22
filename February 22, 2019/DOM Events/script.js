fcf134e0ea0c63f50e509f7f9c272a7a1741429c
PranavBhatia <bhatiapranav1996@gmail.com>
2019-02-22 11:54:49 -0500

    constraints added

--- February 22, 2019/DOM Events/script.js
+++ February 22, 2019/DOM Events/script.js
@@ -3,7 +3,10 @@ var input = document.getElementById("userInput");
 var ul = document.querySelector("ul");
 
 button.addEventListener("click", function() {
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
+    if (input.value.length > 0) {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(input.value));
+        ul.appendChild(li);
+        input.value = "";
+    }
 })
